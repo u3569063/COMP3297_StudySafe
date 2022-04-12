f872a959d26762bacd40e878bba45169d22cd876
Kolton Ko <m4a79x@icloud.com>
2022-04-12 23:01:34 +0800

    Added classes from Domain Model
    Summary
    =======
    1. Removed Django Secret Key
    2. Created database for the Django App
    3. Added all classes in Domain Model to the model of the app (api)

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Venue(models.Model):
+    venue_code = models.CharField(max_length=20)
+    location = models.CharField(max_length=150)
+    type = models.CharField(max_length=2)
+    capacity = models.IntegerField()
+
+class Member(models.Model):
+    hkuid = models.CharField(max_length=10)
+    name = models.CharField(max_length=150)
+
+class Record(models.Model):
+    hkuid = models.ForeignKey(Member, on_delete=models.CASCADE)
+    venue_code = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    action = models.BinaryField()
