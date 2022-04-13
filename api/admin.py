927d641547fcf4a6bd2b038349509cd47a00f1f5
Kolton Ko <m4a79x@icloud.com>
2022-04-14 07:40:13 +0800

    SS-4 SS-5 Revised model for Venue and Member class
    Summary
    =======
    1. Added reporting string to all classes
    2. venue_code and hkuid is no longer explicitly set as a primary key

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Member)
+admin.site.register(Venue)
+admin.site.register(AccessRecord)
+admin.site.register(PositiveCases)
--- api/models.py
+++ api/models.py
@@ -6,17 +6,25 @@ class Venue(models.Model):
     location = models.CharField(max_length=150)
     type = models.CharField(max_length=2)
     capacity = models.IntegerField()
+    def __str__(self):
+        return self.location
 
 class Member(models.Model):
     hkuid = models.CharField(max_length=10)
     name = models.CharField(max_length=150)
+    def __str__(self):
+        return self.name
 
-class Record(models.Model):
+class AccessRecord(models.Model):
     hkuid = models.ForeignKey(Member, on_delete=models.CASCADE)
     venue_code = models.ForeignKey(Venue, on_delete=models.CASCADE)
     date_time = models.DateTimeField()
-    action = models.BinaryField()
+    action = models.CharField(max_length=5)
+    def __str__(self):
+        return f"{self.hkuid}: {self.venue_code} {self.action} @ {self.date_time}"
 
 class PositiveCases(models.Model):
     hkuid = models.ForeignKey(Member, on_delete=models.CASCADE)
-    date_of_diagnosis = models.DateTimeField()
+    date_of_diagnosis = models.DateField()
+    def __str__(self):
+        return f"{self.hkuid}: {self.date_of_diagnosis}"
