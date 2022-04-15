76e6efc498b590a7cb25a2a9fa69a99aafe0fe1c
Kolton Ko <m4a79x@icloud.com>
2022-04-15 21:42:28 +0800

    SS-23 Moved all views to api_view

--- api/api_views.py
+++ api/api_views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from datetime import datetime, timedelta
 from .models import *
 from .serializers import *
+from StudySafeCore.settings import DEBUG
 
 ENTRY = "entry"
 EXIT = "exit"
@@ -14,53 +15,29 @@ def stayed_for_more_than_30_mins(entry_time, exit_time):
     return True if exit_time - entry_time >= timedelta(minutes=30) else False
 
 def list_close_contacts():
-    disinfect_venue = list()
+    close_contact_ids = list()
+    for case in PositiveCase.objects.all():
+        access_record = AccessRecord.objects.filter(HKU_ID=case.HKU_ID)
+        for visit in access_record:
+            if (visit.Date_Time.date() - case.Date_Of_Diagnosis) <= timedelta(days=2):
+                for person in AccessRecord.objects.filter(Venue_Code=visit.Venue_Code):
+                    if (person.Date_Time.date() - case.Date_Of_Diagnosis) <= timedelta(days=2):
+                        if not close_contact_ids.__contains__(person.HKU_ID):
+                            close_contact_ids.append(person.HKU_ID)
+    # close_contact_names = Member.objects.filter(HKU_ID__in=close_contact_ids).order_by("HKU_ID")
+    close_contact_names = list()
+    for person in Member.objects.all().order_by("HKU_ID"):
+        if close_contact_ids.__contains__(person):
+            close_contact_names.append(person)
 
-    positive_cases = PositiveCase.objects.all()
-    for case in positive_cases:
-        record = AccessRecord.objects.filter(HKU_ID=case.HKU_ID)
-        for visit in record:
-            visit_date = visit.Date_Time.date()
-            positive_date = case.Date_Of_Diagnosis
-            if visited_in_past_2_days(visit_date, positive_date):
-                # The Venue_Code of visit is suspicious
-                # see if stayed for more than 30 mins
-                entry_times = (
-                    record.filter(HKU_ID=case.HKU_ID)
-                        .filter(Action=ENTRY)
-                        .values('Date_Time')
-                        .order_by('Date_Time')
-                )
-                exit_times = (
-                    record.filter(HKU_ID=case.HKU_ID)
-                        .filter(Action=EXIT)
-                        .values('Date_Time')
-                        .order_by('Date_Time')
-                )
-                # close contacts to be
-                # for i, en_t in enumerate(entry_times):
-                #     # if someone tested positive have not leave the venue
-                #     if i not in range(len(exit_times)):
-                #         if stayed_for_more_than_30_mins(datetime.now(), en_t):
-                #             # check close contacts who left after him
-
-                #         # still need to check remaining records
-                #         continue
-                        
-                #     elif stayed_for_more_than_30_mins(exit_times[i], en_t):
-                #         disinfect_venue.append(visit.Venue_Code.pk)
-                        
-
-
-    return
+    return close_contact_names
 
 def list_disinfect_venues():
     # For each positive case, check whether they have visited any venues within 2 days
     disinfect_venue = list()
-
     positive_cases = PositiveCase.objects.all()
     for case in positive_cases:
-        record = AccessRecord.objects.filter(HKU_ID=case.HKU_ID)
+        record = AccessRecord.objects.filter(HKU_ID=case.HKU_ID).filter(Action=ENTRY)
         for visit in record:
             visit_date = visit.Date_Time.date()
             positive_date = case.Date_Of_Diagnosis
@@ -68,26 +45,26 @@ def list_disinfect_venues():
                 # The Venue_Code of visit is suspicious
                 # see if stayed for more than 30 mins
                 entry_times = (
-                    record.filter(HKU_ID=case.HKU_ID)
-                        .filter(Action=ENTRY)
-                        .values('Date_Time')
+                    record.values('Date_Time')
                         .order_by('Date_Time')
                 )
                 exit_times = (
-                    record.filter(HKU_ID=case.HKU_ID)
+                    AccessRecord.objects.filter(HKU_ID=case.HKU_ID)
                         .filter(Action=EXIT)
                         .values('Date_Time')
                         .order_by('Date_Time')
                 )
+               
                 for i, en_t in enumerate(entry_times):
                     # if someone tested positive have not leave the venue
                     if i not in range(len(exit_times)):
-                        if stayed_for_more_than_30_mins(datetime.now().astimezone(), en_t["Date_Time"]):
+                        timeDiff = datetime.now().astimezone()-en_t["Date_Time"]
+                        if timeDiff >= timedelta(minutes=30):
                             disinfect_venue.append(visit.Venue_Code.pk)
                         # still need to check remaining records
                         continue
-                        
-                    elif stayed_for_more_than_30_mins(exit_times[i], en_t):
+                    timeDiff = exit_times[i]["Date_Time"]-en_t["Date_Time"]
+                    if timeDiff >= timedelta(minutes=30):
                         disinfect_venue.append(visit.Venue_Code.pk)
 
     disinfect_venue = Venue.objects.filter(Venue_Code__in=disinfect_venue).order_by("Venue_Code")
@@ -98,9 +75,9 @@ class DisinfectVenueViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = list_disinfect_venues()
     serializer_class = VenueSerializer
 
-# class CloseContactViewSet(viewsets.ReadOnlyModelViewSet):
-#     queryset = list_close_contacts()
-#     serializer_class = MemberSerializer
+class CloseContactViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = list_close_contacts()
+    serializer_class = MemberSerializer
 
 class RecordViewSet(viewsets.ModelViewSet):
     queryset = AccessRecord.objects.all()
--- api/tests.py
+++ api/tests.py
@@ -1,4 +1,5 @@
-from datetime import timedelta
+from argparse import Action
+from datetime import datetime, timedelta
 from django.test import TestCase
 
 
@@ -7,15 +8,52 @@ from django.test import TestCase
 # List all
 from api.models import *
 from datetime import timedelta
+ENTRY = "entry"
+EXIT = "exit"
 
-class ListAsQuerySet(list):
 
-    def __init__(self, *args, model, **kwargs):
-        self.model = model
-        super().__init__(*args, **kwargs)
+def visited_in_past_2_days(visit_date, test_date):
+    return True if visit_date - test_date <= timedelta(days=2) else False
 
-    def filter(self, *args, **kwargs):
-        return self  # filter ignoring, but you can impl custom filter
+def stayed_for_more_than_30_mins(entry_time, exit_time):
+    return True if exit_time - entry_time >= timedelta(minutes=30) else False
 
-    def order_by(self, *args, **kwargs):
-        return self
+def list_disinfect_venues():
+    # For each positive case, check whether they have visited any venues within 2 days
+    disinfect_venue = list()
+    positive_cases = PositiveCase.objects.all()
+    for case in positive_cases:
+        record = AccessRecord.objects.filter(HKU_ID=case.HKU_ID).filter(Action=ENTRY)
+        for visit in record:
+            visit_date = visit.Date_Time.date()
+            positive_date = case.Date_Of_Diagnosis
+            if visited_in_past_2_days(visit_date, positive_date):
+                # The Venue_Code of visit is suspicious
+                # see if stayed for more than 30 mins
+                entry_times = (
+                    record.values('Date_Time')
+                        .order_by('Date_Time')
+                )
+                exit_times = (
+                    AccessRecord.objects.filter(HKU_ID=case.HKU_ID)
+                        .filter(Action=EXIT)
+                        .values('Date_Time')
+                        .order_by('Date_Time')
+                )
+               
+                for i, en_t in enumerate(entry_times):
+                    # if someone tested positive have not leave the venue
+                    if i not in range(len(exit_times)):
+                        timeDiff = datetime.now().astimezone()-en_t["Date_Time"]
+                        if timeDiff >= timedelta(minutes=30):
+                            disinfect_venue.append(visit.Venue_Code.pk)
+                        # still need to check remaining records
+                        continue
+                    timeDiff = exit_times[i]["Date_Time"]-en_t["Date_Time"]
+                    if timeDiff >= timedelta(minutes=30):
+                        disinfect_venue.append(visit.Venue_Code.pk)
+
+    disinfect_venue = Venue.objects.filter(Venue_Code__in=disinfect_venue).order_by("Venue_Code")
+    return disinfect_venue
+
+print(list_disinfect_venues())
