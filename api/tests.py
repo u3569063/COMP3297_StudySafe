4f0591b1736f5325004c058a6da96c68155b27c9
Hayward Chan <u3568604@connect.hku.hk>
2022-04-17 04:10:13 +0800

    SS-21 Fixed logic for disinfection venues list

--- api/tests.py
+++ api/tests.py
@@ -1,4 +1,5 @@
 from argparse import Action
+from asyncio.windows_events import NULL
 from datetime import datetime, timedelta
 from django.test import TestCase
 
@@ -23,35 +24,26 @@ def list_disinfect_venues():
     disinfect_venue = list()
     positive_cases = PositiveCase.objects.all()
     for case in positive_cases:
-        record = AccessRecord.objects.filter(HKU_ID=case.HKU_ID).filter(Action=ENTRY)
-        for visit in record:
+        record = AccessRecord.objects.filter(HKU_ID=case.HKU_ID).filter(Action=ENTRY).order_by('Date_Time')
+        for i in range(len(record)):
+            visit = record[i]
             visit_date = visit.Date_Time.date()
             positive_date = case.Date_Of_Diagnosis
             if visited_in_past_2_days(visit_date, positive_date):
                 # The Venue_Code of visit is suspicious
                 # see if stayed for more than 30 mins
-                entry_times = (
-                    record.values('Date_Time')
-                        .order_by('Date_Time')
-                )
-                exit_times = (
+                record_e = (
                     AccessRecord.objects.filter(HKU_ID=case.HKU_ID)
                         .filter(Action=EXIT)
-                        .values('Date_Time')
                         .order_by('Date_Time')
                 )
-               
-                for i, en_t in enumerate(entry_times):
-                    # if someone tested positive have not leave the venue
-                    if i not in range(len(exit_times)):
-                        timeDiff = datetime.now().astimezone()-en_t["Date_Time"]
-                        if timeDiff >= timedelta(minutes=30):
-                            disinfect_venue.append(visit.Venue_Code.pk)
-                        # still need to check remaining records
-                        continue
-                    timeDiff = exit_times[i]["Date_Time"]-en_t["Date_Time"]
-                    if timeDiff >= timedelta(minutes=30):
-                        disinfect_venue.append(visit.Venue_Code.pk)
+                entry_t, exit_t = visit.Date_Time, 0
+                if i not in range(len(record_e)):
+                    exit_t = datetime.now().astimezone()
+                else:
+                    exit_t = record_e[i].Date_Time
+                if stayed_for_more_than_30_mins(entry_t, exit_t):
+                    disinfect_venue.append(visit.Venue_Code.pk)
 
     disinfect_venue = Venue.objects.filter(Venue_Code__in=disinfect_venue).order_by("Venue_Code")
     return disinfect_venue
